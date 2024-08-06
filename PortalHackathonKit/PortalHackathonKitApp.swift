12c50bc17e55f24f7e1f2e3ae60ed02ac5e4d6f4
Ahmed Ragab Issa <AhmedRagabIssa@gmail.com>
2024-08-06 21:38:58 +0300

    Initial Commit

---
+++ PortalHackathonKit/PortalHackathonKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalHackathonKitApp.swift
+//  PortalHackathonKit
+//
+//  Created by Ahmed Ragab on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PortalHackathonKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
